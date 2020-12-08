12ccbef04da722d613cc18816b8d379568e87488
gmvbr <72897639+gmvbr@users.noreply.github.com>
2020-12-08 11:27:34 -0300

    update autoload, add blipp

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import fastify from 'fastify';
+
+import blipp from 'fastify-blipp';
 import autoload from 'fastify-autoload';
+
 import {join} from 'path';
 
 import {Config, Configuration} from './config';
@@ -11,12 +14,15 @@ declare module 'fastify' {
 }
 
 export async function bootstrap() {
-  fastify()
+  const app = fastify()
     .decorate('config', Config)
+    .register(blipp)
     .register(autoload, {
       dir: join(__dirname, 'services'),
-    })
-    .listen(Config.PORT!, Config.HOST!);
+      ignorePattern: /.*(test|spec).js/,
+    });
+  app.listen(Config.PORT!, Config.HOST!);
+  app.blipp();
 }
 
 bootstrap();
