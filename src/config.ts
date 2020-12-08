bd8b1902d22d86e990327ecc773d7105444adb2e
gmvbr <72897639+gmvbr@users.noreply.github.com>
2020-12-08 11:48:09 -0300

    update config

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import envSchema from 'env-schema';
 
 const schema = {
   type: 'object',
-  required: ['PORT', 'HOST'],
+  required: ['PORT', 'HOST', 'JWT_SECRET'],
   properties: {
     PORT: {
       type: 'string',
@@ -12,12 +12,16 @@ const schema = {
       type: 'string',
       default: '0.0.0.0',
     },
+    JWT_SECRET: {
+      type: 'string',
+    },
   },
 };
 
 export interface Configuration {
   PORT?: string;
   HOST?: string;
+  JWT_SECRET?: string;
 }
 
 export const Config: Configuration = envSchema({
